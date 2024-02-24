f20ed0ddc6051488276061fe10be57133f33f0d5
Derlonger <add.email@mail.ru>
2024-02-24 08:04:18 +0300

    done

--- pattern/04_command.go
+++ pattern/04_command.go
@@ -1,7 +1,113 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «комманда».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Command_pattern
 */
+
+/*
+Паттерн Command позволяет инкапсулировать запрос на выполнение определенного действия в виде отдельного объекта.
+Этот объект запроса на действие и называется 'командой'
+
+Плюсы:
+
+	1. Инкапсуляция действий:
+		- Команда позволяет инкапсулировать запрос на выполнение действия в виде отдельного объекта.
+		- Это упрощает добавление новых операций без изменения самих объектов.
+	2. Отделение инициатора и получателя:
+		- Объекты, инициирующие запросы на выполнение действия (инициаторы), отделяются от объектов,
+	которые выполняют это действие (получатели).
+	3. Поддержка отмены операций:
+		-Команды могут поддерживать отмену выполненных действий (например, через метод Undo()).
+
+Минусы:
+
+	1.Сложность:
+		- Внедрение паттерна может усложнить код, особенно если у нас большое количество команд и получателей.
+	2. Нарушение инкапсуляции:
+		- Получатель команды (например, класс Receiver) получает доступ к приватным членам объектов, что может нарушить инкапсуляцию.
+	3. Увеличение числа классов:
+		- Для каждой операции требуется создание отдельной команды, что может привести к увеличению числа классов в системе.
+
+*/
+
+// Интерфейс команды
+type Command interface {
+	Execute() // Выполнение команды
+	Undo()    // Отмена команды
+}
+
+// Конкретная команда для добавления книги
+type AddBookCommand struct {
+	receiver *BookReceiver
+	book     Book
+}
+
+func (c *AddBookCommand) Execute() {
+	c.receiver.AddBook(c.book)
+}
+
+func (c *AddBookCommand) Undo() {
+	c.receiver.RemoveBook(c.book)
+}
+
+// Получатель команды
+type BookReceiver struct {
+	books []Book
+}
+
+func (r *BookReceiver) AddBook(book Book) {
+	r.books = append(r.books, book)
+	fmt.Println("Книга добавлена:", book.Title)
+}
+
+func (r *BookReceiver) RemoveBook(book Book) {
+	index := -1
+	for i, b := range r.books {
+		if b.Title == book.Title {
+			index = i
+			break
+		}
+	}
+
+	if index != -1 {
+		r.books = append(r.books[:index], r.books[index+1:]...)
+		fmt.Println("Книга удалена:", book.Title)
+	} else {
+		fmt.Println("Книга не найдена:", book.Title)
+	}
+}
+
+// Структура книги
+type Book struct {
+	Title string
+}
+
+// Инициатор команды
+type Invoker struct {
+	command Command
+}
+
+func (i *Invoker) SetCommand(command Command) {
+	i.command = command
+}
+
+func (i *Invoker) Run() {
+	i.command.Execute()
+}
+
+func main() {
+	receiver := &BookReceiver{}
+	addCommand := &AddBookCommand{
+		receiver: receiver,
+		book:     Book{Title: "Гарри Поттер и философский камень"},
+	}
+
+	// Инициатор команды
+	invoker := &Invoker{}
+	invoker.SetCommand(addCommand)
+	invoker.Run()
+}
--- pattern/05_chain_of_resp.go
+++ pattern/05_chain_of_resp.go
@@ -1,7 +1,134 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «цепочка вызовов».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Chain-of-responsibility_pattern
 */
+
+/*
+Chain of responsibility(Цепочка вызовов) - это поведенческий паттерн проектирования, который позволяет передавать задачи
+по цепочке объектов. Каждый объект в цепочке может обработать запрос или передать его дальше.
+
+Плюсы:
+
+	1. Разделение обязанностей:
+		- Позволяет разделить обязанности между объектами, каждый из которых выполняет свою часть работы.
+	2. Гибкость и расширяемость:
+		- Мы можем легко добавлять новые обработчики в цепочку без изменения существующего кода.
+	3. Избегание жестких зависимостей:
+		- Объекты в цепочке не зависят друг от друга напрямую, что делает систему более гибкой.
+
+Минусы:
+	1. Нет гарантии обработки:
+		- Если ни один из обработчиков не обработал запрос, он может остаться без ответа. Необходимо обеспечить,
+		чтобы в цепочке всегда был обработчик, способный обработать запрос.
+	2. Потенциальная перегрузка:
+		- Если цепочка слишком длинная или обработчики выполняют сложные операции,
+		это может привести к нежелательной нагрузке на систему.
+	3. Сложность отладки:
+		- При наличии множества обработчиков может быть сложно определить, где именно возникла ошибка.
+
+*/
+
+// OrderHandler - Интерфейс для обработки заказов
+type OrderHandler interface {
+	SetNext(handler OrderHandler)
+	Handle(order Order)
+}
+
+// Order - структура заказа
+type Order struct {
+	ID       int
+	Products []string
+}
+
+// ProductAvailabilityHandler - обработчик проверки наличия товара
+type ProductAvailabilityHandler struct {
+	next OrderHandler
+}
+
+func (h *ProductAvailabilityHandler) SetNext(handler OrderHandler) {
+	h.next = handler
+}
+
+func (h *ProductAvailabilityHandler) Handle(order Order) {
+	// Проверка наличия товара
+	fmt.Println("Проверка наличия товара для заказа", order.ID)
+	// Если товар есть, передаем обработку следующему обработчику
+	if h.next != nil {
+		h.next.Handle(order)
+	}
+}
+
+// CostCalculationHandler - обработчик расчета стоимости
+type CostCalculationHandler struct {
+	next OrderHandler
+}
+
+func (h *CostCalculationHandler) SetNext(handler OrderHandler) {
+	h.next = handler
+}
+
+func (h *CostCalculationHandler) Handle(order Order) {
+	// Рассчитываем стоимость заказа
+	fmt.Println("Расчет стоимости заказа", order.ID)
+	// Передаем обработку следующему обработчику
+	if h.next != nil {
+		h.next.Handle(order)
+	}
+}
+
+// OrderConfirmationHandler - обработчик оформления заказа
+type OrderConfirmationHandler struct {
+	next OrderHandler
+}
+
+func (h *OrderConfirmationHandler) SetNext(handler OrderHandler) {
+	h.next = handler
+}
+
+func (h *OrderConfirmationHandler) Handle(order Order) {
+	// Оформляем заказ
+	fmt.Println("Оформление заказа", order.ID)
+	// Передаем обработку следующему обработчику
+	if h.next != nil {
+		h.next.Handle(order)
+	}
+}
+
+// NotificationHandler - обработчик отправки уведомления клиенту
+type NotificationHandler struct {
+	next OrderHandler
+}
+
+func (h *NotificationHandler) SetNext(handler OrderHandler) {
+	h.next = handler
+}
+
+func (h *NotificationHandler) Handle(order Order) {
+	// Отправляем уведомление клиенту
+	fmt.Println("Отправка уведомления клиенту о заказе", order.ID)
+}
+
+func main() {
+	order := Order{
+		ID:       123,
+		Products: []string{"item1", "item2"},
+	}
+
+	// Создаем цепочку обработчиков
+	productAvailabilityHandler := &ProductAvailabilityHandler{} // Проверка наличия товара
+	costCalculationHandler := &CostCalculationHandler{}         // расчет стоимости
+	orderConfirmationHandler := &OrderConfirmationHandler{}     // оформление заказа
+	notificationHandler := &NotificationHandler{}               // Отправка уведомления пользователю
+
+	productAvailabilityHandler.SetNext(costCalculationHandler)
+	costCalculationHandler.SetNext(orderConfirmationHandler)
+	orderConfirmationHandler.SetNext(notificationHandler)
+
+	// Запускаем обработку заказа
+	productAvailabilityHandler.Handle(order)
+}
